de3235414c67a0de653fa80055f018c369ebe4fe
J Fixby <github@jfixby.com>
2017-05-27 19:08:37 +0200

    +unit oper

---
+++ red-triplane-api-ui/src/com/jfixby/r3/api/ui/unit/UnitOperator.java
@@ -0,0 +1,32 @@
+
+package com.jfixby.r3.api.ui.unit;
+
+import com.jfixby.scarabei.api.ComponentInstaller;
+import com.jfixby.scarabei.api.assets.ID;
+
+public class UnitOperator {
+
+	static private ComponentInstaller<UnitOperatorComponent> componentInstaller = new ComponentInstaller<UnitOperatorComponent>(
+		"UnitOperator");
+
+	public static final void installComponent (final UnitOperatorComponent component_to_install) {
+		componentInstaller.installComponent(component_to_install);
+	}
+
+	public static void installComponent (final String className) {
+		componentInstaller.installComponent(className);
+	}
+
+	public static final UnitOperatorComponent invoke () {
+		return componentInstaller.invokeComponent();
+	}
+
+	public static final UnitOperatorComponent component () {
+		return componentInstaller.getComponent();
+	}
+
+	public static void deployUnit (final ID unitID) {
+		invoke().deployUnit(unitID);
+	}
+
+}
---
+++ red-triplane-api-ui/src/com/jfixby/r3/api/ui/unit/UnitOperatorComponent.java
@@ -0,0 +1,10 @@
+
+package com.jfixby.r3.api.ui.unit;
+
+import com.jfixby.scarabei.api.assets.ID;
+
+public interface UnitOperatorComponent {
+
+	void deployUnit (ID unitID);
+
+}
